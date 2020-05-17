4bc0674cb350a4763da33e1a6866a4ac982feb31
Mátyás Fodor <matyas.fodor@gmail.com>
2020-05-17 19:42:33 +0100

    chore: prepare for publishing

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
-chrome-tabcounter
-=================
+A fork of (asafh/chrome-tabcounter)[asafh/chrome-tabcounter] - Super simple tab/window count indicator for Chrome.
 
-Super simple tab/window count indicator for Chrome
+The purpose of this tool is to provide a color-coded feedback when your tab count gets out of hand.
+
+Switch between modes by clicking on the extension icon:
+
+- Show tab count
+- Show window count
+- Show tab and window count
+
+The color of the icon is determined by the number of tabs.
+
+Demo:
+
+![Tab counter demo](media/demo.gif)
 
 ## License
+
 Dual licensed under [MIT](http://opensource.org/licenses/MIT) and [Apache 2.0](http://www.apache.org/licenses/LICENSE-2.0) licenses
--- background.js
+++ background.js
@@ -1,150 +1,148 @@
 "use strict";
 
 function Counter(name, listener) {
-    this.count = 0;
-    this.name = name;
-    this.listener = listener;
+  this.count = 0;
+  this.name = name;
+  this.listener = listener;
 }
-Counter.prototype.setCount = function(count) {
-    this.count = count;
-    this.listener(this);
+Counter.prototype.setCount = function (count) {
+  this.count = count;
+  this.listener(this);
 };
-Counter.prototype.increment = function() {
-    this.setCount(this.count+1);
+Counter.prototype.increment = function () {
+  this.setCount(this.count + 1);
 };
-Counter.prototype.decrement = function() {
-    this.setCount(this.count-1);
+Counter.prototype.decrement = function () {
+  this.setCount(this.count - 1);
 };
 function TabCounter(listener) {
-    Counter.call(this,"Tabs",listener);
-    var self = this;
-    chrome.tabs.query({}, function(tabs) {
-        self.setCount(tabs.length);
-    });
-    chrome.tabs.onCreated.addListener(self.increment.bind(self));
-    chrome.tabs.onRemoved.addListener(self.decrement.bind(self));
+  Counter.call(this, "Tabs", listener);
+  var self = this;
+  chrome.tabs.query({}, function (tabs) {
+    self.setCount(tabs.length);
+  });
+  chrome.tabs.onCreated.addListener(self.increment.bind(self));
+  chrome.tabs.onRemoved.addListener(self.decrement.bind(self));
 }
 TabCounter.prototype = new Counter();
 
 function WindowCounter(listener) {
-    Counter.call(this,"Windows",listener);
-    var self = this;
-    chrome.windows.getAll(function(windows) {
-        self.setCount(windows.length);
-    });
-    chrome.windows.onCreated.addListener(self.increment.bind(self));
-    chrome.windows.onRemoved.addListener(self.decrement.bind(self));
+  Counter.call(this, "Windows", listener);
+  var self = this;
+  chrome.windows.getAll(function (windows) {
+    self.setCount(windows.length);
+  });
+  chrome.windows.onCreated.addListener(self.increment.bind(self));
+  chrome.windows.onRemoved.addListener(self.decrement.bind(self));
 }
 WindowCounter.prototype = new Counter();
 
-
 function onUpdate() {
-    var text = "",
-        title = mode,
-        count = 0,
-        backgroundColor = null,
-        fontColor = null;
+  const text =
+    mode == "Windows"
+      ? `${counters.Windows.count}`
+      : mode == "Tabs"
+      ? `${counters.Tabs.count}`
+      : mode == "Both"
+      ? `${counters.Tabs.count} / ${counters.Windows.count}`
+      : "";
 
-    if(mode !== "Windows") {
-        text += counters.Tabs.count;
-        count = counters.Tabs.count;
-    }
-    if(mode === "Both") {
-        text += "/";
-        title = "Tabs over Windows";
-    }
-    if(mode !== "Tabs") {
-        text += counters.Windows.count;
-        count = counters.Windows.count;
-    }
+  const title = mode !== "Both" ? mode : "Tabs over Windows";
 
-    backgroundColor = heatMapColorforValue(scaleCount(count));
-    fontColor = contrastColor(...backgroundColor);
+  const backgroundColor = heatMapColorforValue(scaleCount(counters.Tabs.count));
+  const fontColor = contrastColor(...backgroundColor);
 
-    draw({backgroundColor: backgroundColor, fontColor: fontColor, text: text});
-    chrome.browserAction.setTitle({title: title});
+  draw({ backgroundColor, fontColor, text });
+  chrome.browserAction.setTitle({ title });
 }
 
 function scaleCount(value) {
-    return Math.pow(1.0 - 1.0 / Math.log(value + 3), 0.3);
+  return 1.0 - 1.0 / (value / 3.0 + 1);
 }
 
 function heatMapColorforValue(value) {
-    var h = ((1.0 - value) * 240.0) / 355.0
-    return hslToRgb(h, 1, 0.5);
+  var h = ((1.0 - value) * 240.0) / 355.0;
+  return hslToRgb(h, 1, 0.5);
 }
 
-function hslToRgb(h, s, l){
-    var r, g, b;
+function hslToRgb(h, s, l) {
+  var r, g, b;
 
-    if (s === 0) {
-        r = g = b = l; // achromatic
-    } else {
-        const hue2rgb = function (p, q, t) {
-            if(t < 0) t += 1;
-            if(t > 1) t -= 1;
-            if(t < 1/6) return p + (q - p) * 6 * t;
-            if(t < 1/2) return q;
-            if(t < 2/3) return p + (q - p) * (2 / 3 - t) * 6;
-            return p;
-        }
+  if (s === 0) {
+    r = g = b = l; // achromatic
+  } else {
+    const hue2rgb = function (p, q, t) {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1 / 6) return p + (q - p) * 6 * t;
+      if (t < 1 / 2) return q;
+      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+      return p;
+    };
 
-        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        var p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1/3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1/3);
-    }
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1 / 3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1 / 3);
+  }
 
-    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
 }
+
+const BLACK = [0, 0, 0];
+const WHITE = [255, 255, 255];
 
 const contrastColor = (r, g, b) => {
-    const a = 1 - ( 0.299 * r + 0.587 * g + 0.114 * b) / 255.0;
-    return a < 0.5 ? [0, 0, 0] : [255, 255, 255];
+  const a = 1 - (0.299 * r + 0.587 * g + 0.114 * b) / 255.0;
+  return a < 0.5 ? BLACK : WHITE;
 };
 
-function componentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
+function uint8ToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
 }
 
 function rgbToHex(r, g, b) {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  return [r, g, b].reduce(
+    (hexa, nextColor) => `${hexa}${uint8ToHex(nextColor)}`,
+    "#"
+  );
 }
 
-function draw(obj) {
-    var canvas = document.createElement('canvas');
-    canvas.width = 19;
-    canvas.height = 19;
+const ICON_SIZE = 19;
 
-    var context = canvas.getContext('2d');
-    context.fillStyle = rgbToHex(...obj.backgroundColor);
-    context.fillRect(0, 0, 19, 19);
+function draw({ backgroundColor, fontColor, text }) {
+  var canvas = document.createElement("canvas");
+  canvas.width = ICON_SIZE;
+  canvas.height = ICON_SIZE;
 
-    context.fillStyle = rgbToHex(...obj.fontColor);
-    context.textAlign = "center";
-    context.textBaseline = "middle";
-    context.font = "bold 11px Arial";
-    context.fillText(obj.text, 9, 9);
+  var context = canvas.getContext("2d");
+  context.fillStyle = rgbToHex(...backgroundColor);
+  context.fillRect(0, 0, ICON_SIZE, ICON_SIZE);
 
-    chrome.browserAction.setIcon({
-      imageData: context.getImageData(0, 0, 19, 19)
-    });
-  }
+  context.fillStyle = rgbToHex(...fontColor);
+  context.textAlign = "center";
+  context.textBaseline = "middle";
+  context.font = "bold 11px Arial";
+  context.fillText(text, ICON_SIZE / 2, ICON_SIZE / 2);
+
+  chrome.browserAction.setIcon({
+    imageData: context.getImageData(0, 0, ICON_SIZE, ICON_SIZE),
+  });
+}
 
 const counters = {
-    "Tabs": new TabCounter(onUpdate),
-    "Windows": new WindowCounter(onUpdate)
-}
+  Tabs: new TabCounter(onUpdate),
+  Windows: new WindowCounter(onUpdate),
+};
 const modes = Object.keys(counters).concat(["Both"]);
 var mode = modes[0];
 
-
-chrome.browserAction.onClicked.addListener(function() {
-    const nextIndex = (modes.indexOf(mode) + 1) % modes.length;
-    mode = modes[nextIndex];
-    onUpdate();
+chrome.browserAction.onClicked.addListener(function () {
+  const nextIndex = (modes.indexOf(mode) + 1) % modes.length;
+  mode = modes[nextIndex];
+  onUpdate();
 });
 
 onUpdate();
